ec4f50264aa42545b4c74d533bf9cce981691161
Raphaël Valyi <rvalyi@gmail.com>
2009-10-22 11:26:51 -0200

    [FIX] fix order line discount mapping. Avoid trying to import non existing partners. Indeed Magento order adresses have a parent_id that doesn't point to the customer which is different from OpenERP

--- sale.py
+++ sale.py
@@ -97,12 +97,16 @@ class sale_order(magerp_osv.magerp_osv):
     }
     
     def oevals_from_extdata(self, cr, uid, external_referential_id, data_record, key_field, mapping_lines, defaults, context):
+        if data_record.get('billing_address', False):
+            del(data_record['billing_address']['parent_id'])
+            del(data_record['shipping_address']['parent_id'])
         res = super(magerp_osv.magerp_osv, self).oevals_from_extdata(cr, uid, external_referential_id, data_record, key_field, mapping_lines, defaults, context)
 
         if data_record.get('billing_address', False):
             address_default = {}
             if res.get('parter_id', False):
                 address_default = {'parter_id': res.get('parter_id', False)}
+
             #TODO unlike Magento, avoid creating same addresses over and over, try to detect if customer already have such an address (Magento won't tell it!)
             inv_res = self.pool.get('res.partner.address').ext_import(cr, uid, [data_record['billing_address']], external_referential_id, address_default, context)
             ship_res = self.pool.get('res.partner.address').ext_import(cr, uid, [data_record['shipping_address']], external_referential_id, address_default, context)
