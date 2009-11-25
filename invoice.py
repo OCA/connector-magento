e46afdf9e4e37c0985482d8c8f616b0c2f5cdc2d
Raphaël Valyi <rvalyi@gmail.com>
2009-11-25 10:32:15 -0200

    [FIX] added missing file

---
+++ invoice.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+#########################################################################
+#This module intergrates Open ERP with the magento core                 #
+#Core settings are stored here                                          #
+#########################################################################
+#                                                                       #
+# Copyright (C) 2009  Sharoon Thomas, Raphaël Valyi                     #
+#                                                                       #
+#This program is free software: you can redistribute it and/or modify   #
+#it under the terms of the GNU General Public License as published by   #
+#the Free Software Foundation, either version 3 of the License, or      #
+#(at your option) any later version.                                    #
+#                                                                       #
+#This program is distributed in the hope that it will be useful,        #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+#GNU General Public License for more details.                           #
+#                                                                       #
+#You should have received a copy of the GNU General Public License      #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+#########################################################################
+
+from osv import osv, fields
+
+class account_invoice(osv.osv):
+    _inherit = "account.invoice"
+    
+    _columns = {
+                    'magento_ref':fields.char('Magento REF', size=32),
+                }
+account_invoice()
