dbe0fad8ef55e6c12bd1fd97c767437433281c4c
Raphaël Valyi <rvalyi@gmail.com>
2009-11-26 02:43:22 -0200

    [REF] revert order payment mapping code as it was already done in CSV declarative style mapping.

--- sale.py
+++ sale.py
@@ -29,8 +29,6 @@ DEBUG = True
 #TODO, may be move that on out CSV mapping, but not sure we can easily
 #see OpenERP sale/sale.py and Magento app/code/core/Mage/Sales/Model/Order.php for details
 ORDER_STATUS_MAPPING = {'draft': 'processing', 'progress': 'processing', 'shipping_except': 'complete', 'invoice_except': 'complete', 'done': 'closed', 'cancel': 'canceled', 'waiting_date': 'holded'}
-PAYMENTS_MAPPING = {'checkmo': 'prepaid', 'ccsave': 'prepaid', 'free':'prepaid','googlecheckout':'prepaid','paypayl_express':'prepaid', 'paybox_system': 'prepaid'}
-
 
 class sale_shop(magerp_osv.magerp_osv):
     _inherit = "sale.shop"
@@ -201,8 +199,6 @@ class sale_order(magerp_osv.magerp_osv):
             res = self.get_order_lines(cr, uid, res, external_referential_id, data_record, key_field, mapping_lines, defaults, context)
             if data_record.get('shipping_amount', False) and float(data_record.get('shipping_amount', False)) > 0:
                 res = self.get_order_shipping(cr, uid, res, external_referential_id, data_record, key_field, mapping_lines, defaults, context)
-        if data_record.get('payment_method', False):
-            res['order_policy'] = PAYMENTS_MAPPING.get(data_record['payment_method'], 'prepaid')
 
         return res
     
