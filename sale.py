003449781f15e29c370916a9ffab6a059bfdb308
Raphaël Valyi <rvalyi@gmail.com>
2009-12-10 02:39:48 -0200

    [IMP] now set the invoice origin field to the Magento ref after creating a Magento invoice eventually

--- sale.py
+++ sale.py
@@ -95,17 +95,17 @@ class sale_shop(magerp_osv.magerp_osv):
             result['status_change'] = conn.call('sales_order.addComment', [ext_id, status, '', True])
         
         #creation of Magento invoice eventually:
-        cr.execute("select account_invoice.id from account_invoice inner join sale_order_invoice_rel on invoice_id = account_invoice.id where  order_id = %s" % order.id)
+        cr.execute("select account_invoice.id from account_invoice inner join sale_order_invoice_rel on invoice_id = account_invoice.id where order_id = %s" % order.id)
         resultset = cr.fetchone()
         if resultset and len(resultset) == 1:
             invoice = self.pool.get("account.invoice").browse(cr, uid, resultset[0])
             if invoice.amount_total == order.amount_total and not invoice.magento_ref:
                 try:
                     result['magento_invoice_ref'] = conn.call('sales_order_invoice.create', [order.magento_incrementid, [], _("Invoice Created"), True, True])
-                    self.pool.get("account.invoice").write(cr, uid, invoice.id, {'magento_ref': result['magento_invoice_ref']})
+                    self.pool.get("account.invoice").write(cr, uid, invoice.id, {'magento_ref': result['magento_invoice_ref'], 'origin': result['magento_invoice_ref']})
                 except Exception, e:
                     pass #TODO make sure that's because Magento invoice already exists and then re-attach it!
-        
+
         return result
 
 sale_shop()
