9157298b32a26de24cc314ac27ca0499ba7d89d7
Vianney - www.akretion.com.br <vianney.dacosta@akretion.com.br>
2010-09-03 18:38:40 -0300

    [FIX] module new dependency of mrp

--- __terp__.py
+++ __terp__.py
@@ -24,6 +24,7 @@
                  'delivery',
                  "base_sale_multichannels",
                  "product_images_olbs",
+                 "mrp",
                 ],
     "author" : "Sharoon Thomas, Raphael Valyi",
     "description": """Magento E-commerce management
