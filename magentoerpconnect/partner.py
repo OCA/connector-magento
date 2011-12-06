2447143384868e786060a1895c1c3b2f5672b2e0
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2011-12-06 15:48:19 +0100

    [IMP] Binding partners on import : Actually when we import a new customer into OpenERP, it search for a partner with the same emailid (filled in advance) and bind it with the Magento's customer. This is defective because on Magento, you can create an account with the same email per website. So now the partner will be searched by the emailid and website, or only emailid if the website has not been filled, but only the first partner not already linked with a magento customer will be bound with the partner. If no partner is found with this conditions, a new partner is created as usual. Also add a constraint on res_partner (emailid, website)
    This needs the update of base_external_referentials in extra-addons because it use the brand new method _existing_oeid_for_extid_import instead of the hook _search_existing_id_by_vals

--- magentoerpconnect/partner.py
+++ magentoerpconnect/partner.py
@@ -51,7 +51,7 @@ class res_partner(magerp_osv.magerp_osv):
     _inherit = "res.partner"
 
     def _is_magento_exported(self, cr, uid, ids, field_name, arg, context=None):
-        """Return True if the product is already exported to at least one magento shop
+        """Return True if the partner is already exported to at least one magento shop
         """
         res = {}
         # get all magento external_referentials
@@ -68,25 +68,53 @@ class res_partner(magerp_osv.magerp_osv):
                     'group_id':fields.many2one('res.partner.category', 'Magento Group(Category)'),
                     'store_id':fields.many2one('magerp.storeviews', 'Last Store View', readonly=True, help="Last store view where the customer has bought."),
                     'store_ids':fields.many2many('magerp.storeviews', 'magerp_storeid_rel', 'partner_id', 'store_id', 'Store Views', readonly=True),
-                    'website_id':fields.many2one('external.shop.group', 'Website'),
+                    'website_id':fields.many2one('external.shop.group', 'Website', help='Select a website for with which the Magento\'s customer will be bound.'),
                     'created_in':fields.char('Created in', size=100),
                     'created_at':fields.datetime('Created Date'),
                     'updated_at':fields.datetime('Updated At'),
-                    'emailid':fields.char('Email Address', size=100, help="Magento uses this email ID to match the customer. If filled, if a Magento customer is imported with the exact same email, he will be bound with this partner and this latter will be updated with Magento's values."),
+                    'emailid':fields.char('Email Address', size=100, help="Magento uses this email ID to match the customer. If filled, if a Magento customer is imported from the selected website with the exact same email, he will be bound with this partner and this latter will be updated with Magento's values."),
                     'mag_vat':fields.char('Magento VAT', size=50, help="To be able to receive customer VAT number you must set it in Magento Admin Panel, menu System / Configuration / Client Configuration / Name and Address Options."),
                     'mag_birthday':fields.date('Birthday', help="To be able to receive customer birthday you must set it in Magento Admin Panel, menu System / Configuration / Client Configuration / Name and Address Options."),
                     'mag_newsletter':fields.boolean('Newsletter'),
                     'magento_exported': fields.function(_is_magento_exported, type="boolean", method=True, string="Exists on Magento"),
                 }
-	
-    def _search_existing_id_by_vals(self, cr, uid, vals, external_id, external_referential_id, defaults=None, context=None):
-        """ 
-            Return: ID of the partner to bind with the external partner id
+
+    _sql_constraints = [('emailid_uniq', 'unique(emailid, website_id)', 'A partner already exists with this email address on the selected website.')]
+
+    def _existing_oeid_for_extid_import(self, cr, uid, vals, external_id, external_referential_id, context=None):
         """
+        When importing a new customer from Magento (no external_id found in ir.model.data), we search
+        for a partner not already linked with magento with the same emailid and website than the imported one
+        (fields must be filled in advance).
+
+        The external customer will update the partner returned in the tuple.
+        The external id will be create on this partner with the external resource if the value is False in the tuple.
+
+        @param vals: vals to create in OpenERP, already evaluated by oevals_from_extdata
+        @param external_id: external id of the resource to create
+        @param external_referential_id: external referential id from where we import the resource
+        @return: tuple of (ir.model.data id / False: external id to create, model resource id / False: resource to create)
+        """
+        existing_ir_model_data_id, existing_res_id = super(res_partner, self)\
+        ._existing_oeid_for_extid_import(cr, uid, vals, external_id, external_referential_id, context=context)
+
+        if existing_res_id:
+            return existing_ir_model_data_id, existing_res_id
+
         magento_mail = vals['emailid']
-        res_ids = self.search(cr, uid, [('emailid', '=', magento_mail)], context=context)
-        if len(res_ids) > 1:
-            raise osv.except_osv(_('Error'), _("More than one partner found with the email address : %s") % (magento_mail,))
-        return res_ids and res_ids[0] or False
+        website_id = self.pool.get('external.shop.group').\
+        extid_to_existing_oeid(cr, uid, vals['website_id'], external_referential_id, context=context)
+        partner_ids = self.search(cr, uid,
+                                  [('emailid', '=', magento_mail),
+                                   ('website_id', '=', website_id)],
+                                  context=context)
+        # when the partner has been created, the website is not necessarily filled, in such case we search the first occurrence
+        # with the same email not already binded with a magento customer
+        if not partner_ids:
+            partner_ids = self.search(cr, uid, [('emailid', '=', magento_mail), ('website_id', '=', False)], context=context)
+        partner_ids = [partner.id for partner
+                       in self.browse(cr, uid, partner_ids[:], context=context)
+                       if not partner.magento_exported]
+        return False, partner_ids and partner_ids[0] or False
 
 res_partner()
