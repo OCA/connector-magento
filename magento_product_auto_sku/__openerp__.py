b6685fbf303ac5ac32046ad50f8543316212e43b
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2012-02-28 10:41:02 +0100

    [FIX] licenses

--- magento_product_auto_sku/__openerp__.py
+++ magento_product_auto_sku/__openerp__.py
@@ -1,19 +1,20 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author Guewen Baconnier. Copyright Camptocamp SA
+#    Author: Guewen Baconnier
+#    Copyright 2011-2012 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -22,6 +23,7 @@
     'version' : '1.0',
     'depends' : ['base', 'product', 'magentoerpconnect'],
     'author' : 'Camptocamp',
+    'license': 'AGPL-3',
     'description': """Auto incrementation of the Magento SKU column on products""",
     'website': 'http://www.camptocamp.com',
     'init_xml': [],
