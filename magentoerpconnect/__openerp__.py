e31b7795a70475e70c0eaf7a2ae6424f8f54c98b
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-04-17 15:52:43 +0200

    [ADD] description in the manifest __openerp__.py

--- magentoerpconnect/__openerp__.py
+++ magentoerpconnect/__openerp__.py
@@ -37,7 +37,75 @@
 Magento Connector
 =================
 
-Use the **Connector** module to synchronize OpenERP with Magento.
+This is the new release of the Open-Source connector linking OpenERP and
+Magento also known under the name of **Magentoerpconnect**.  It is
+build on top of the `connector`_ framework. It is is structured so that
+it can be extended or modified easily from separate addons, a factor of
+success when the implementations of Magento vary a lot.
+
+Magento OpenERP Connector is mainly developed by the Magentoerpconnect Core
+Editors, these being Camptocamp_ and Akretion_. The `source is on
+launchpad`_.
+
+This connector is designed to have a strong and efficient core, with the
+ability to extend it with extension modules or local customizations.
+
+It features:
+
+Synchronizations:
+
+* Import the partners and addresses book
+* Import the customer groups (becomes partner tags)
+* Import the categories of products, with translations
+* Import the products, with translations and main image
+* Import the sales orders
+* Update the the stock quantities,
+  with configuration of the warehouse and stock field to use
+* Export the delivery orders
+* Export the tracking numbers
+* Create the invoices on Magento and get their number back
+* Import the dependencies when they are not yet imported
+  (ie. customer, products for sale order)
+
+Automatizations:
+
+* Use the `Automatic workflows` to automatize the workflow of the sales
+  according to the payment method (confirm orders, create and reconcile
+  payments, ...)
+* Per payment method, choose when the orders are imported
+  (only when a payment is received, always, never)
+* Use the `Sales Exceptions` to prevents the processing of sales orders
+  with issues
+
+Technical points:
+
+* Built on top of the `connector`_ framework
+* Use the `connector_ecommerce`_ addon to share the e-commerce capabilities
+  with other e-commerce addons
+* Use the jobs system of the `connector`_ framework
+* Create `connector`_ checkpoints when new records to verify are imported
+* Support Magento 1.7, the support of earlier versions should be easy to
+  add, the `connector`_ framework being designed to handle multiple
+  versions with ease.
+* Documentation-included (help's welcome)
+* Licensed under AGPL version 3
+* Designed to be usable with multiple Magento or any other e-commerce backends
+  in the same time.
+
+Available extensions:
+
+* Pricing
+        allows to manage the prices in OpenERP using pricelists,
+        prices are update to Magento when changed
+* Export of partners (Experimental)
+        Export new partners on Magento, not complete.
+
+
+.. _connector: https://code.launchpad.net/openerp-connector
+.. _connector_ecommerce: https://code.launchpad.net/openerp-connector
+.. _Camptocamp: http://www.camptocamp.com
+.. _Akretion: http://www.akretion.com
+.. _`source is on launchpad`: https://launchpad.net/magentoerpconnect
 
 """,
  # TODO change images
