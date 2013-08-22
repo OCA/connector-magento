5c2e644504de21d81479cc86aff7a2166253fb12
Augustin Cisterne-Kaas <augustin.cisternekaas@elicocorp.com>
2013-08-22 14:58:15 +0800

    [FIX] Improved push of products (../connector-magentoconnect-catalog rev 683.1.226)

--- magentoerpconnect_catalog/product.py
+++ magentoerpconnect_catalog/product.py
@@ -11,7 +11,7 @@ from openerp.addons.magentoerpconnect.unit.export_synchronizer import (
         MagentoExporter)
 from openerp.addons.magentoerpconnect.backend import magento
 from openerp.addons.magentoerpconnect.product import ProductProductAdapter
-
+from openerp.addons.connector.exception import MappingError
 
 @magento
 class ProductProductDeleteSynchronizer(MagentoDeleteSynchronizer):
@@ -30,13 +30,36 @@ class ProductProductExportMapper(ExportMapper):
     # direct = [('name', 'name'),
     #           ('description', 'description'),
     #           ('weight', 'weight'),
-    #           ('standard_price', 'cost'),
+    #           ('list_price', 'price'),
     #           ('description_sale', 'short_description'),
     #           ('default_code', 'sku'),
     #           ('product_type', 'type'),
     #           ('created_at', 'created_at'),
     #           ('updated_at', 'updated_at'),
+    #           ('status', 'status'),
+    #           ('visibility', 'visibility'),
+    #           ('product_type', 'product_type')
     #           ]
+    @mapping
+    def all(self, record):
+        return {'name': record.name,
+                'description': record.description,
+                'weight': record.weight,
+                'price': record.list_price,
+                'short_description': record.description_sale,
+                'type': record.product_type,
+                'created_at': record.created_at,
+                'updated_at': record.updated_at,
+                'status': record.status,
+                'visibility': record.visibility,
+                'product_type': record.product_type }
+
+    @mapping
+    def sku(self, record):
+        sku = record.default_code
+        if not sku:
+            raise MappingError("The product attribute default code cannot be empty.")
+        return {'sku': sku}
 
     @mapping
     def set(self, record):
@@ -45,24 +68,28 @@ class ProductProductExportMapper(ExportMapper):
         return {'attrset': set_id}
 
     @mapping
+    def tax(self, record):
+        tax_class_id = record.tax_class if record.tax_class else 0
+        return {'tax_class_id': tax_class_id}
+
+    @mapping
+    def website_ids(self, record):
+        website_ids = []
+        for website_id in record.website_ids:
+            magento_id = website_id.magento_id
+            website_ids.append(magento_id)
+        return {'website_ids': website_ids}
+
+    @mapping
     def category(self, record): 
         categ_ids = []
-        binder = self.get_binder_for_model('magento.product.category')
-        for categ in record.categ_ids:
-            categ_id = binder.to_backend(categ.id)
-            categ_ids.append(categ_id)
+        if record.categ_id:
+            for m_categ in record.categ_id.magento_bind_ids:
+                if m_categ.backend_id.id == self.backend_record.id:
+                    categ_ids.append(m_categ.magento_id) 
 
-        return {
-            'categories': categ_ids,
-            'name': record.name,
-            'description': record.description,
-            'weight': record.weight,
-            'price': record.list_price,
-            'short_description': record.description,
-            'sku': record.default_code,
-            'product_type': record.product_type,
-            'website_ids': [1],
-            'status': 1,
-            'visibility': 4,
-            'tax_class_id':4,
-            }
+        for categ in record.categ_ids:
+            for m_categ in categ.magento_bind_ids:
+                if m_categ.backend_id.id == self.backend_record.id:
+                    categ_ids.append(m_categ.magento_id)            
+        return {'categories': categ_ids}
