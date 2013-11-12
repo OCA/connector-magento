974524dacf830ea7586c27d228e9a6ca85a1d60e
Sebastien Beau <sebastien.beau@akretion.com>
2013-11-12 11:12:36 +0100

    [IMP] add export of dependency for category (../connector-magentoconnect-catalog rev 704)

--- magentoerpconnect_catalog/product_category.py
+++ magentoerpconnect_catalog/product_category.py
@@ -145,6 +145,21 @@ class ProductCategoryDeleteSynchronizer(MagentoDeleteSynchronizer):
 class ProductCategoryExport(MagentoExporter):
     _model_name = ['magento.product.category']
 
+    def _export_dependencies(self):
+        """Export parent of the category"""
+        env = self.environment
+        record = self.binding_record
+        binder = self.get_binder_for_model()
+        if record.magento_parent_id:
+            mag_parent_id = record.magento_parent_id.id
+            if binder.to_backend(mag_parent_id) is None:
+                exporter = env.get_connector_unit(MagentoExportSynchronizer)
+                exporter.run(mag_parent_id)
+        elif record.openerp_id.parent_id:
+            parent = record.openerp_id.parent_id
+            if binder.to_backend(parent.id, wrap=True) is None:
+                exporter = env.get_connector_unit(MagentoExportSynchronizer)
+                exporter.run(parent.magento_parent_id.id)
 
 @magento
 class ProductCategoryExportMapper(ExportMapper):
