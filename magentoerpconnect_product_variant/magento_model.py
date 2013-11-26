ff7df1c5df632f348cbd8651390047af214f7f53
Sebastien Beau <sebastien.beau@akretion.com>
2013-11-26 10:41:24 +0100

    [FIX] add missing file (../connector-magentoconnect-catalog rev 726)

---
+++ magentoerpconnect_product_variant/magento_model.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP 
+#   Copyright (C) 2013 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp.osv import fields, orm
+
+
+class magento_backend(orm.Model):
+    _inherit = 'magento.backend'
+
+    _columns = {
+        'export_simple_product_on_fly': fields.boolean('Export Simple Product Onfly',
+            help=('Tic this option if you always when to export all simple '
+            'product related to your configurable when you export a '
+            'configurable product. Default value will be use for generating '
+            'the binding')
+            ),
+    }
