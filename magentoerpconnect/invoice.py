acaf46f6c4942f40260ca7e599cb4ec9cc5ea53f
Jan-Philipp Fischer <jan-philipp.fischer@greencoding.de>
2014-01-13 14:26:35 +0100

    Bugfixes related to indentation

--- magentoerpconnect/invoice.py
+++ magentoerpconnect/invoice.py
@@ -214,7 +214,6 @@ def invoice_create_bindings(session, model_name, record_id):
     # find the magento store to retrieve the backend
     # we use the shop as many sale orders can be related to an invoice
     for sale in invoice.sale_ids:
-		payment_method = sale.payment_method_id
         for magento_sale in sale.magento_bind_ids:
             # Check if invoice state matches configuration setting
             # for when to export an invoice
@@ -224,11 +223,12 @@ def invoice_create_bindings(session, model_name, record_id):
                                  None)
             assert magento_store
 			
-			payment_method = sale.payment_method_id
-			if payment_method and payment_method.create_invoice_on:
-				create_invoice = payment_method.create_invoice_on
-			else:
-				create_invoice = magento_store.create_invoice_on
+	    payment_method = sale.payment_method_id
+	    if payment_method and payment_method.create_invoice_on:
+            	create_invoice = payment_method.create_invoice_on
+	    else:
+		create_invoice = magento_store.create_invoice_on	    
+
 
             if create_invoice == invoice.state:
                 session.create('magento.account.invoice',
