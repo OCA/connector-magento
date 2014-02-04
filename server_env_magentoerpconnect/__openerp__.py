aba3cf96ae6c4110d9942b3bb698eb6cfd8a0dfa
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-02-04 13:07:15 +0100

    formatting of manifest

--- server_env_magentoerpconnect/__openerp__.py
+++ server_env_magentoerpconnect/__openerp__.py
@@ -19,9 +19,9 @@
 #
 ##############################################################################
 
-{'name' : 'Server environment for Magento Connector',
- 'version' : '1.0',
- 'author' : 'Camptocamp',
+{'name': 'Server environment for Magento Connector',
+ 'version': '1.0',
+ 'author': 'Camptocamp',
  'maintainer': 'Camptocamp',
  'license': 'AGPL-3',
  'category': 'Tools',
@@ -35,19 +35,21 @@ Server environment for Magento Connector
 ========================================
 
 This module is based on the `server_environment` module to use files for
-configuration.  Thus we can have a different file for each environment
-(dev, test, staging, prod).  This module define the config variables
-for the `magentoerpconnect` module.  In the configuration file,
-you can configure the url, login and password of the Magento Backends.
+configuration.  Thus we can have a different configutation for each
+environment (dev, test, staging, prod).  This module define the config
+variables for the `magentoerpconnect` module.
 
-Exemple of the section to put in the configuration file :
+In the configuration file, you can configure the url, login and
+password of the Magento Backends.
 
-[magento_backend.name_of_the_backend]
-location = http://localhost/magento/
-username = my_api_login
-password = my_api_password
+Exemple of the section to put in the configuration file::
 
- """,
+    [magento_backend.name_of_the_backend]
+    location = http://localhost/magento/
+    username = my_api_login
+    password = my_api_password
+
+""",
  'website': 'http://www.camptocamp.com',
  'data': [],
  'test': [],
