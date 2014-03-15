837a094b72b66b591fe070fd8e32312189b9d967
David Béal <david.beal@akretion.com>
2014-03-15 17:28:50 +0100

    [ADD] add test on ...order_comment

---
+++ magentoerpconnect_order_comment/tests/test_synchronization.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Benoit GUILLOT
+#    Copyright 2014 Akretion
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+#import unittest2
+#from functools import partial
+
+#from openerp.addons.connector.exception import InvalidDataError
+from openerp.addons.magentoerpconnect.tests.test_synchronization import test_base_magento
+from openerp.addons.magentoerpconnect.unit.import_synchronizer import (
+    import_batch,
+    import_record)
+from openerp.addons.magentoerpconnect.tests.common import (
+    mock_api,
+    mock_urlopen_image)
+from .test_data_comment import magento_comment_responses
+
+
+class test_import_magento_sale_comment(test_base_magento):
+    """ Test the imports from a Magento Mock.
+
+    The data returned by Magento are those created for the
+    demo version of Magento on a standard 1.7 version.
+    """
+
+    def test_10_import_sale_comment(self):
+        """ Test import of sale order comment"""
+        backend_id = self.backend_id
+        with mock_api(magento_comment_responses):
+            with mock_urlopen_image():
+                import_record(self.session,
+                              'magento.sale.order',
+                              backend_id, '100000006')
+        order_model = self.registry('magento.sale.order')
+        mag_order_ids = order_model.search(
+            self.cr, self.uid,
+            [('backend_id', '=', backend_id),
+             ('magento_id', '=', '100000006')])
+        self.assertEqual(len(mag_order_ids), 1)
+
+        order_id = order_model.read(
+            self.cr, self.uid, mag_order_ids[0], ['openerp_id'])['openerp_id']
+        comment_ids = self.registry('magento.sale.comment').search(
+            self.cr, self.uid,
+            [('backend_id', '=', backend_id),
+             ('res_id', '=', order_id)])
+        self.assertEqual(len(comment_ids), 1)
+#TODO text import None comment
+
+
+class test_export_magento_sale_comment(test_base_magento):
+    """ Test the imports from a Magento Mock.
+
+    The data returned by Magento are those created for the
+    demo version of Magento on a standard 1.7 version.
+    """
+
+    def test_10_export_sale_comment(self):
+        """ Test export of sale order comment"""
+        
