ef55fa32cdc49f29b88a3f24ffba943601ce97e3
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-04-17 10:38:34 +0200

    backend_id on 'magento.sale.order' is *not* a related, we must keep it

--- magentoerpconnect/sale.py
+++ magentoerpconnect/sale.py
@@ -807,6 +807,10 @@ class SaleOrderImportMapper(ImportMapper):
     # are done in the importer
 
     @mapping
+    def backend_id(self, record):
+        return {'backend_id': self.backend_record.id}
+
+    @mapping
     def user_id(self, record):
         """ Do not assign to a Salesperson otherwise sales orders are hidden
         for the salespersons (access rules)"""
