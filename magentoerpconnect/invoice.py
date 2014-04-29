46d9b12393244e6331a1526a7fec6d6a31969907
David Béal <david.beal@akretion.com>
2014-04-29 16:11:59 +0200

    [FIX] prevent create a second binding with the same backend

--- magentoerpconnect/invoice.py
+++ magentoerpconnect/invoice.py
@@ -213,28 +213,30 @@ def invoice_create_bindings(session, model_name, record_id):
     invoice = session.browse(model_name, record_id)
     # find the magento store to retrieve the backend
     # we use the shop as many sale orders can be related to an invoice
-    if not invoice.magento_bind_ids:
-        for sale in invoice.sale_ids:
-            for magento_sale in sale.magento_bind_ids:
-                # Check if invoice state matches configuration setting
-                # for when to export an invoice
-                magento_stores = magento_sale.shop_id.magento_bind_ids
-                magento_store = next((store for store in magento_stores
-                                      if store.backend_id.id == magento_sale.backend_id.id),
-                                     None)
-                assert magento_store
+    for sale in invoice.sale_ids:
+        for magento_sale in sale.magento_bind_ids:
+            for mag_inv in invoice.magento_bind_ids:
+                if mag_inv.backend_id.id == magento_sale.backend_id.id:
+                    continue
+            # Check if invoice state matches configuration setting
+            # for when to export an invoice
+            magento_stores = magento_sale.shop_id.magento_bind_ids
+            magento_store = next((store for store in magento_stores
+                                  if store.backend_id.id == magento_sale.backend_id.id),
+                                 None)
+            assert magento_store
 
-                payment_method = sale.payment_method_id
-                if payment_method and payment_method.create_invoice_on:
-                    create_invoice = payment_method.create_invoice_on
-                else:
-                    create_invoice = magento_store.create_invoice_on
+            payment_method = sale.payment_method_id
+            if payment_method and payment_method.create_invoice_on:
+                create_invoice = payment_method.create_invoice_on
+            else:
+                create_invoice = magento_store.create_invoice_on
 
-                if create_invoice == invoice.state:
-                    session.create('magento.account.invoice',
-                                   {'backend_id': magento_sale.backend_id.id,
-                                    'openerp_id': invoice.id,
-                                    'magento_order_id': magento_sale.id})
+            if create_invoice == invoice.state:
+                session.create('magento.account.invoice',
+                               {'backend_id': magento_sale.backend_id.id,
+                                'openerp_id': invoice.id,
+                                'magento_order_id': magento_sale.id})
 
 
 @on_record_create(model_names='magento.account.invoice')
