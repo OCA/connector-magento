0dd35a0811a757e9e288003e57430e9dca88832c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-05-06 11:04:55 +0200

    long line

--- magentoerpconnect/product.py
+++ magentoerpconnect/product.py
@@ -102,7 +102,11 @@ class magento_product_product(orm.Model):
         'magento_qty': fields.float('Computed Quantity',
                                     help="Last computed quantity to send "
                                          "on Magento."),
-        'no_stock_sync': fields.boolean('No Stock Synchronization', help="Check this to exclude the product from stock synchronizations.", required=False)
+        'no_stock_sync': fields.boolean(
+            'No Stock Synchronization',
+            required=False,
+            help="Check this to exclude the product "
+                 "from stock synchronizations."),
         }
 
     _defaults = {
