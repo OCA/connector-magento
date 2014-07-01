77c6de40036d8906093b23a8d9768d6a4471f4d8
Sebastien Beau <sebastien.beau@akretion.com>
2014-07-02 00:22:51 +0200

    [IMP] add test for attribute set synchronisation

---
+++ magentoerpconnect_catalog/tests/test_data.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   Module for OpenERP
+#   Copyright (C) 2014 Akretion (http://www.akretion.com).
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+magento_attribute_responses = \
+{('ol_catalog_product_attributeset.info', (9,)): {'attribute_set_id': '9',
+                                                  'attribute_set_name': 'Default',
+                                                  'entity_type_id': '10',
+                                                  'sort_order': '16'},
+ ('ol_catalog_product_attributeset.info', (38,)): {'attribute_set_id': '38',
+                                                   'attribute_set_name': 'Cell Phones',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (39,)): {'attribute_set_id': '39',
+                                                   'attribute_set_name': 'Computer',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (40,)): {'attribute_set_id': '40',
+                                                   'attribute_set_name': 'Shoes',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (41,)): {'attribute_set_id': '41',
+                                                   'attribute_set_name': 'Shirts T',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (42,)): {'attribute_set_id': '42',
+                                                   'attribute_set_name': 'Furniture',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (44,)): {'attribute_set_id': '44',
+                                                   'attribute_set_name': 'Cameras',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (45,)): {'attribute_set_id': '45',
+                                                   'attribute_set_name': 'Shirts Other',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (46,)): {'attribute_set_id': '46',
+                                                   'attribute_set_name': 'Shirts (General)',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (58,)): {'attribute_set_id': '58',
+                                                   'attribute_set_name': 'RAM',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (59,)): {'attribute_set_id': '59',
+                                                   'attribute_set_name': 'Warranties',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (60,)): {'attribute_set_id': '60',
+                                                   'attribute_set_name': 'CPU',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (61,)): {'attribute_set_id': '61',
+                                                   'attribute_set_name': 'Monitors',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (62,)): {'attribute_set_id': '62',
+                                                   'attribute_set_name': 'Hard Drive',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.info', (63,)): {'attribute_set_id': '63',
+                                                   'attribute_set_name': 't-shirt-open',
+                                                   'entity_type_id': '10',
+                                                   'sort_order': '0'},
+ ('ol_catalog_product_attributeset.search', ()): ['44',
+                                                  '38',
+                                                  '39',
+                                                  '60',
+                                                  '9',
+                                                  '42',
+                                                  '62',
+                                                  '61',
+                                                  '58',
+                                                  '46',
+                                                  '45',
+                                                  '41',
+                                                  '40',
+                                                  '63',
+                                                  '59']}
