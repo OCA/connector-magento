b3c5112c01976b426db37432f4e94abed70ad93e
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-07-04 13:33:10 +0200

    Correct test data for bundle products

--- magentoerpconnect/tests/test_data_product.py
+++ magentoerpconnect/tests/test_data_product.py
@@ -35,7 +35,7 @@ import urllib2
 
 # a simple product with images
 simple_product_and_images = {
- ('catalog_product.info', (122, None, None, 'id')): {'categories': ['1'],
+ ('ol_catalog_product.info', (122, None, None, 'id')): {'categories': ['1'],
                                                      'color': '60',
                                                      'cost': '2.0000',
                                                      'country_of_manufacture': None,
