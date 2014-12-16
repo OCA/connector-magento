83ffadb81a259b273101ab9350862146a6e1362f
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2014-12-16 11:13:50 +0100

    Better way to check for value = None

--- magentoerpconnect/sale.py
+++ magentoerpconnect/sale.py
@@ -992,15 +992,11 @@ class SaleOrderLineImportMapper(ImportMapper):
 
     @mapping
     def discount_amount(self, record):
-        discount_value = 0.0
-        row_total = 0.0
-        if record.get('discount_amount'):
-            discount_value = float(record.get('discount_amount', 0))
-        if self.backend_record.catalog_price_tax_included and \
-           record.get('row_total_incl_tax'):
-            row_total = float(record.get('row_total_incl_tax', 0))
-        elif record.get('row_total'):
-            row_total = float(record.get('row_total', 0))
+        discount_value = float(record.get('discount_amount') or 0)
+        if self.backend_record.catalog_price_tax_included:
+            row_total = float(record.get('row_total_incl_tax') or 0)
+        else:
+            row_total = float(record.get('row_total') or 0)
         discount = 0
         if discount_value > 0 and row_total > 0:
             discount = 100 * discount_value / row_total
