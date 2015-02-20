82d6d1ea8d9d627e22c75e51de1bf9dae2604d7b
jssuzanne <jssuzanne@anybox.fr>
2015-02-20 15:16:23 +0100

    [REF] test_address_book testcase, migrated in v8 api

--- magentoerpconnect/tests/test_address_book.py
+++ magentoerpconnect/tests/test_address_book.py
@@ -42,29 +42,25 @@ class test_import_address_book(common.SingleTransactionCase):
 
     def setUp(self):
         super(test_import_address_book, self).setUp()
-        self.backend_model = self.registry('magento.backend')
-        self.session = ConnectorSession(self.cr, self.uid)
-        self.session.context['__test_no_commit'] = True
-        self.model = self.registry('magento.res.partner')
-        self.address_model = self.registry('magento.address')
+        self.backend_model = self.env['magento.backend']
+        context = dict(self.env.context, __test_no_commit=True)
+        self.session = ConnectorSession(self.env.cr, self.env.uid,
+                                        context=context)
+        self.model = self.env['magento.res.partner']
+        self.address_model = self.env['magento.address']
         backend_ids = self.backend_model.search(
-            self.cr, self.uid,
             [('name', '=', 'Test Magento Address book')])
         if backend_ids:
-            self.backend_id = backend_ids[0]
+            self.backend_id = backend_ids[0].id
         else:
-            data_obj = self.registry('ir.model.data')
-            warehouse_id = data_obj.get_object_reference(
-                self.cr, self.uid, 'stock', 'warehouse0')[1]
-            self.backend_id = self.backend_model.create(
-                self.cr,
-                self.uid,
-                {'name': 'Test Magento Address book',
-                 'version': '1.7',
-                 'location': 'http://anyurl',
-                 'username': 'guewen',
-                 'warehouse_id': warehouse_id,
-                 'password': '42'})
+            warehouse_id = self.env.ref('stock.warehouse0').id
+            self.backend_id = self.backend_model.create({
+                'name': 'Test Magento Address book',
+                'version': '1.7',
+                'location': 'http://anyurl',
+                'username': 'guewen',
+                'warehouse_id': warehouse_id,
+                'password': '42'}).id
 
     def test_00_setup(self):
         """ Import the informations required for the customers """
@@ -80,12 +76,10 @@ class test_import_address_book(common.SingleTransactionCase):
         with mock_api(no_address):
             import_record(self.session, 'magento.res.partner',
                           self.backend_id, '9999253')
-        cr, uid = self.cr, self.uid
-        partner_ids = self.model.search(cr, uid,
-                                        [('magento_id', '=', '9999253'),
+        partner_ids = self.model.search([('magento_id', '=', '9999253'),
                                          ('backend_id', '=', self.backend_id)])
         self.assertEqual(len(partner_ids), 1)
-        partner = self.model.browse(cr, uid, partner_ids[0])
+        partner = partner_ids[0]
         self.assertEqual(partner.name, 'Benjamin Le Goff')
         self.assertEqual(partner.type, 'default')
         self.assertEqual(len(partner.child_ids), 0)
@@ -95,12 +89,10 @@ class test_import_address_book(common.SingleTransactionCase):
         with mock_api(individual_1_address):
             import_record(self.session, 'magento.res.partner',
                           self.backend_id, '9999254')
-        cr, uid = self.cr, self.uid
-        partner_ids = self.model.search(cr, uid,
-                                        [('magento_id', '=', '9999254'),
+        partner_ids = self.model.search([('magento_id', '=', '9999254'),
                                          ('backend_id', '=', self.backend_id)])
         self.assertEqual(len(partner_ids), 1)
-        partner = self.model.browse(cr, uid, partner_ids[0])
+        partner = partner_ids[0]
         # Name of the billing address
         self.assertEqual(partner.name, 'Ferreira Margaux')
         self.assertEqual(partner.type, 'default')
@@ -118,12 +110,10 @@ class test_import_address_book(common.SingleTransactionCase):
         with mock_api(individual_2_addresses):
             import_record(self.session, 'magento.res.partner',
                           self.backend_id, '9999255')
-        cr, uid = self.cr, self.uid
-        partner_ids = self.model.search(cr, uid,
-                                        [('magento_id', '=', '9999255'),
+        partner_ids = self.model.search([('magento_id', '=', '9999255'),
                                          ('backend_id', '=', self.backend_id)])
         self.assertEqual(len(partner_ids), 1)
-        partner = self.model.browse(cr, uid, partner_ids[0])
+        partner = partner_ids[0]
         # Name of the billing address
         self.assertEqual(partner.name, u'Mace Sébastien')
         self.assertEqual(partner.type, 'default')
@@ -145,12 +135,10 @@ class test_import_address_book(common.SingleTransactionCase):
         with mock_api(company_1_address):
             import_record(self.session, 'magento.res.partner',
                           self.backend_id, '9999256')
-        cr, uid = self.cr, self.uid
-        partner_ids = self.model.search(cr, uid,
-                                        [('magento_id', '=', '9999256'),
+        partner_ids = self.model.search([('magento_id', '=', '9999256'),
                                          ('backend_id', '=', self.backend_id)])
         self.assertEqual(len(partner_ids), 1)
-        partner = self.model.browse(cr, uid, partner_ids[0])
+        partner = partner_ids[0]
         # Company of the billing address
         self.assertEqual(partner.name, 'Marechal')
         self.assertEqual(partner.type, 'default')
@@ -167,12 +155,10 @@ class test_import_address_book(common.SingleTransactionCase):
         with mock_api(company_2_addresses):
             import_record(self.session, 'magento.res.partner',
                           self.backend_id, '9999257')
-        cr, uid = self.cr, self.uid
-        partner_ids = self.model.search(cr, uid,
-                                        [('magento_id', '=', '9999257'),
+        partner_ids = self.model.search([('magento_id', '=', '9999257'),
                                          ('backend_id', '=', self.backend_id)])
         self.assertEqual(len(partner_ids), 1)
-        partner = self.model.browse(cr, uid, partner_ids[0])
+        partner = partner_ids[0]
         # Company of the billing address
         self.assertEqual(partner.name, 'Bertin')
         self.assertEqual(partner.type, 'default')
@@ -183,11 +169,10 @@ class test_import_address_book(common.SingleTransactionCase):
 
         def get_address(magento_id):
             address_ids = self.address_model.search(
-                cr, uid,
                 [('magento_id', '=', magento_id),
                  ('backend_id', '=', self.backend_id)])
             self.assertEqual(len(address_ids), 1)
-            return self.address_model.browse(cr, uid, address_ids[0])
+            return address_ids[0]
         # billing address
         address = get_address('9999257')
         self.assertEqual(address.type, 'invoice',
