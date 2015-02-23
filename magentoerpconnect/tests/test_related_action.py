fcf951ce25b11569f8e8ebded02638b7d3e3b660
jssuzanne <jssuzanne@anybox.fr>
2015-02-23 19:30:50 +0100

    [MIG] unittest related action to V8

--- magentoerpconnect/tests/test_related_action.py
+++ magentoerpconnect/tests/test_related_action.py
@@ -20,58 +20,51 @@ class test_related_action_storage(common.TransactionCase):
 
     def setUp(self):
         super(test_related_action_storage, self).setUp()
-        cr, uid = self.cr, self.uid
-        backend_model = self.registry('magento.backend')
-        self.session = ConnectorSession(cr, uid)
-        self.session.context['__test_no_commit'] = True
-        warehouse_id = self.ref('stock.warehouse0')
-        backend_id = backend_model.create(
-            cr,
-            uid,
+        context = dict(self.env.context, __test_no_commit=True)
+        backend_model = self.env['magento.backend']
+        self.session = ConnectorSession(self.env.cr, self.env.uid,
+                                        context=context)
+        warehouse = self.env.ref('stock.warehouse0')
+        self.backend = backend_model.create(
             {'name': 'Test Magento',
                 'version': '1.7',
                 'location': 'http://anyurl',
                 'username': 'username',
-                'warehouse_id': warehouse_id,
+                'warehouse_id': warehouse.id,
                 'password': '42'})
-        self.backend = backend_model.browse(cr, uid, backend_id)
         # import the base informations
         with mock_api(magento_base_responses):
-            import_batch(self.session, 'magento.website', backend_id)
-            import_batch(self.session, 'magento.store', backend_id)
-            import_batch(self.session, 'magento.storeview', backend_id)
-        self.MagentoProduct = self.registry('magento.product.product')
-        self.QueueJob = self.registry('queue.job')
+            import_batch(self.session, 'magento.website', self.backend.id)
+            import_batch(self.session, 'magento.store', self.backend.id)
+            import_batch(self.session, 'magento.storeview', self.backend.id)
+        self.MagentoProduct = self.env['magento.product.product']
+        self.QueueJob = self.env['queue.job']
 
     def test_unwrap_binding(self):
         """ Open a related action opening an unwrapped binding """
-        cr, uid = self.cr, self.uid
-        product_id = self.ref('product.product_product_7')
-        magento_product_id = self.MagentoProduct.create(
-            cr, uid,
-            {'openerp_id': product_id,
+        product = self.env.ref('product.product_product_7')
+        magento_product = self.MagentoProduct.create(
+            {'openerp_id': product.id,
              'backend_id': self.backend.id})
         stored = self._create_job(export_record, 'magento.product.product',
-                                  magento_product_id)
+                                  magento_product.id)
         expected = {
             'name': mock.ANY,
             'type': 'ir.actions.act_window',
             'view_type': 'form',
             'view_mode': 'form',
-            'res_id': product_id,
+            'res_id': product.id,
             'res_model': 'product.product',
         }
         self.assertEquals(stored.open_related_action(), expected)
 
     def _create_job(self, func, *args):
-        cr, uid = self.cr, self.uid
         job = Job(func=func, args=args)
         storage = OpenERPJobStorage(self.session)
         storage.store(job)
-        stored_ids = self.QueueJob.search(self.cr, self.uid,
-                                          [('uuid', '=', job.uuid)])
-        self.assertEqual(len(stored_ids), 1)
-        return self.QueueJob.browse(cr, uid, stored_ids[0])
+        storeds = self.QueueJob.search([('uuid', '=', job.uuid)])
+        self.assertEqual(len(storeds), 1)
+        return storeds[0]
 
     def test_link(self):
         """ Open a related action opening an url on Magento """
