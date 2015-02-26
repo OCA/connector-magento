ca2ce09f0ea9c4322ed300c9f2bd56ff50c270a4
Guewen Baconnier <guewen@gmail.com>
2015-02-26 20:24:27 +0100

    Remove inactive listeners

--- magentoerpconnect/consumer.py
+++ magentoerpconnect/consumer.py
@@ -19,21 +19,12 @@
 #
 ##############################################################################
 
-from openerp.addons.connector.event import (on_record_write,
-                                            on_record_create,
-                                            on_record_unlink
-                                            )
 from openerp.addons.connector.connector import Binder
 from .unit.export_synchronizer import export_record
 from .unit.delete_synchronizer import export_delete_record
 from .connector import get_environment
 
-_MODEL_NAMES = ()
-_BIND_MODEL_NAMES = ()
 
-
-@on_record_create(model_names=_BIND_MODEL_NAMES)
-@on_record_write(model_names=_BIND_MODEL_NAMES)
 def delay_export(session, model_name, record_id, vals):
     """ Delay a job which export a binding record.
 
@@ -46,7 +37,6 @@ def delay_export(session, model_name, record_id, vals):
     export_record.delay(session, model_name, record_id, fields=fields)
 
 
-@on_record_write(model_names=_MODEL_NAMES)
 def delay_export_all_bindings(session, model_name, record_id, vals):
     """ Delay a job which export all the bindings of a record.
 
@@ -64,7 +54,6 @@ def delay_export_all_bindings(session, model_name, record_id, vals):
                             fields=fields)
 
 
-@on_record_unlink(model_names=_BIND_MODEL_NAMES)
 def delay_unlink(session, model_name, record_id):
     """ Delay a job which delete a record on Magento.
 
