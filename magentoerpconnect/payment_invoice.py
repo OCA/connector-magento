e146c07934437d21284a6abdf2f6fe9e23185e3f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-02-27 17:05:58 +0100

    New API for payment.method

--- magentoerpconnect/payment_invoice.py
+++ magentoerpconnect/payment_invoice.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import orm, fields
+from openerp import models, fields
 
 
-class payment_invoice(orm.Model):
+class PaymentMethod(models.Model):
     _inherit = "payment.method"
 
-    _columns = {
-        'create_invoice_on': fields.selection(
-            [('open', 'Validate'),
-             ('paid', 'Paid')],
-            'Create invoice on action',
-            help="Should the invoice be created in Magento "
-                 "when it is validated or when it is paid in OpenERP?\n"
-                 "If nothing is set, the option falls back to the same option "
-                 "on the Magento store related to the sales order."),
-    }
+    create_invoice_on = fields.Selection(
+        selection=[('open', 'Validate'),
+                   ('paid', 'Paid')],
+        string='Create invoice on action',
+        help="Should the invoice be created in Magento "
+             "when it is validated or when it is paid in OpenERP?\n"
+             "If nothing is set, the option falls back to the same option "
+             "on the Magento store related to the sales order.",
+    )
