cb46a23087c1c33f19293e8a7f1eb84cf374dfa1
Damien Crier <damien.crier@camptocamp.com>
2015-11-27 14:26:24 +0100

    [FIX] flake8

--- magentoerpconnect_catalog_simple/models/magento_model/common.py
+++ magentoerpconnect_catalog_simple/models/magento_model/common.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from openerp import api, models, fields
+from openerp import models, fields
 
 
 class MagentoBackend(models.Model):
