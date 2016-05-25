2757caca01f4d026e020a7e3ab867c2181f654ab
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-05-25 16:34:19 +0530

    [IMP] used simple headers & README.rst updated

--- magentoerpconnect/sale.py
+++ magentoerpconnect/sale.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Joel Grand-Guillaume
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2013 Guewen Baconnier,Camptocamp SA,Akretion
+# © 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
 import xmlrpclib
@@ -36,9 +20,11 @@ from openerp.addons.connector.unit.mapper import (mapping,
                                                   )
 from openerp.addons.connector_ecommerce.unit.sale_order_onchange import (
     SaleOrderOnChange)
-from openerp.addons.connector_ecommerce.sale import (ShippingLineBuilder,
-                                                     CashOnDeliveryLineBuilder,
-                                                     GiftOrderLineBuilder)
+
+from openerp.addons.connector_ecommerce.unit.line_builder import (
+    ShippingLineBuilder,
+    CashOnDeliveryLineBuilder,
+    GiftOrderLineBuilder)
 from .unit.backend_adapter import (GenericAdapter,
                                    MAGENTO_DATETIME_FORMAT,
                                    )
