7d0c67fa6d83863d782e37eff5320bf8a0c40766
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-06-30 17:38:20 +0200

    Rewrite tests using new API and vcr.py
    * Use vcr.py to record the exchanges instead of a self-made recorder
    * Use new base Components test case classes
    * Separate export tests in 2 phases: trigger of the export which check
      that the jobs are delayed, and test the job itself in a second test

--- connector_magento/tests/test_related_action.py
+++ connector_magento/tests/test_related_action.py
@@ -1,41 +1,18 @@
 # -*- coding: utf-8 -*-
+# Copyright 2014-2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 import mock
 
-import openerp
-import openerp.tests.common as common
-from openerp.addons.connector.queue.job import (
-    Job,
-    OpenERPJobStorage,
-)
-from openerp.addons.connector.session import (
-    ConnectorSession)
-from .common import mock_api
-from .data_base import magento_base_responses
-from ..unit.import_synchronizer import import_batch, import_record
-from ..unit.export_synchronizer import export_record
+from .common import MagentoSyncTestCase
+from odoo import exceptions
 
 
-class TestRelatedActionStorage(common.TransactionCase):
+class TestRelatedActionStorage(MagentoSyncTestCase):
     """ Test related actions on stored jobs """
 
     def setUp(self):
         super(TestRelatedActionStorage, self).setUp()
-        backend_model = self.env['magento.backend']
-        self.session = ConnectorSession(self.env.cr, self.env.uid,
-                                        context=self.env.context)
-        warehouse = self.env.ref('stock.warehouse0')
-        self.backend = backend_model.create(
-            {'name': 'Test Magento',
-             'version': '1.7',
-             'location': 'http://anyurl',
-             'username': 'username',
-             'warehouse_id': warehouse.id,
-             'password': '42'})
-        # import the base informations
-        with mock_api(magento_base_responses):
-            import_batch(self.session, 'magento.website', self.backend.id)
-            import_batch(self.session, 'magento.store', self.backend.id)
-            import_batch(self.session, 'magento.storeview', self.backend.id)
         self.MagentoProduct = self.env['magento.product.product']
         self.QueueJob = self.env['queue.job']
 
@@ -43,10 +20,11 @@ class TestRelatedActionStorage(common.TransactionCase):
         """ Open a related action opening an unwrapped binding """
         product = self.env.ref('product.product_product_7')
         magento_product = self.MagentoProduct.create(
-            {'openerp_id': product.id,
+            {'odoo_id': product.id,
              'backend_id': self.backend.id})
-        stored = self._create_job(export_record, 'magento.product.product',
-                                  magento_product.id)
+        job = magento_product.with_delay().export_record()
+        stored = job.db_record()
+
         expected = {
             'name': mock.ANY,
             'type': 'ir.actions.act_window',
@@ -57,19 +35,14 @@ class TestRelatedActionStorage(common.TransactionCase):
         }
         self.assertEquals(stored.open_related_action(), expected)
 
-    def _create_job(self, func, *args):
-        job = Job(func=func, args=args)
-        storage = OpenERPJobStorage(self.session)
-        storage.store(job)
-        stored = self.QueueJob.search([('uuid', '=', job.uuid)])
-        self.assertEqual(len(stored), 1)
-        return stored
-
     def test_link(self):
         """ Open a related action opening an url on Magento """
         self.backend.write({'admin_location': 'http://www.example.com/admin'})
-        stored = self._create_job(import_record, 'magento.product.product',
-                                  self.backend.id, 123456)
+        job = self.MagentoProduct.with_delay().import_record(
+            self.backend, '123456'
+        )
+        stored = job.db_record()
+
         url = 'http://www.example.com/admin/catalog_product/edit/id/123456'
         expected = {
             'type': 'ir.actions.act_url',
@@ -81,8 +54,10 @@ class TestRelatedActionStorage(common.TransactionCase):
     def test_link_no_location(self):
         """ Related action opening an url, admin location is not configured """
         self.backend.write({'admin_location': False})
-        self.backend.refresh()
-        stored = self._create_job(import_record, 'magento.product.product',
-                                  self.backend.id, 123456)
-        with self.assertRaises(openerp.exceptions.Warning):
+        job = self.MagentoProduct.with_delay().import_record(
+            self.backend, '123456'
+        )
+        stored = job.db_record()
+        msg = r'No admin URL configured.*'
+        with self.assertRaisesRegexp(exceptions.UserError, msg):
             stored.open_related_action()
