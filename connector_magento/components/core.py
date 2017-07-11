36bc2d5e01ea4a69bff92583d5b89edecde13c80
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-07-11 11:57:16 +0200

    Update documentation

--- connector_magento/components/core.py
+++ connector_magento/components/core.py
@@ -6,6 +6,10 @@ from odoo.addons.component.core import AbstractComponent
 
 
 class BaseMagentoConnectorComponent(AbstractComponent):
+    """ Base Magento Connector Component
+
+    All components of this connector should inherit from it.
+    """
 
     _name = 'base.magento.connector'
     _inherit = 'base.connector'
--- connector_magento/doc/api/api_partner_category.rst
+++
@@ -1,7 +0,0 @@
-Partner Category
-================
-
-.. automodule:: openerp.addons.connector_magento.partner_category
-   :members:
-   :undoc-members:
-   :show-inheritance:
