832510b6ba2b67573100fb75e6f6c059f068437b
Hugo Santos <hugo.santos@factorlibre.com>
2018-03-06 17:59:37 +0100

    [MIG] connector_magento to v11

--- connector_magento/__manifest__.py
+++ connector_magento/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {'name': 'Magento Connector',
- 'version': '10.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'category': 'Connector',
  'depends': ['account',
              'base_technical_user',
