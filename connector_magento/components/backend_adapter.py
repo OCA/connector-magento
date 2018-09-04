9feafbe8ec24a087aac6a414cb694880008b51ee
Florent THOMAS <florent.thomas@mind-and-go.com>
2018-09-04 17:34:44 +0200

    [REF]Avoid attributes to be passed in the read method To be improved

--- connector_magento/components/backend_adapter.py
+++ connector_magento/components/backend_adapter.py
@@ -264,11 +264,13 @@ class GenericAdapter(AbstractComponent):
                     return urllib.quote(term, safe='')
                 return term
 
-            if attributes:
-                raise NotImplementedError
+#             if attributes:
+#                 raise NotImplementedError
             if self._magento2_key:
-                return self._call('%s/%s' % (self._magento2_model, escape(id)),
-                                  attributes)
+                res = self._call('%s/%s' % (self._magento2_model, escape(id)), None)
+                #TODO: loop on attributes to get the json limited values
+                attributes=None
+                return res 
             else:
                 res = self._call(self._magento2_model)
                 return next(record for record in res if record['id'] == id)
