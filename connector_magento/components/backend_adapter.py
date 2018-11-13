f21a7d58caaec2a606d96a5e96cae6605d7129df
Emmanuel HURET <huret.emmanuel@infoatoutprix.fr>
2018-11-13 17:15:47 +0100

    [FIX] search criterea without param

--- connector_magento/components/backend_adapter.py
+++ connector_magento/components/backend_adapter.py
@@ -214,7 +214,7 @@ class GenericAdapter(AbstractComponent):
                 })
                 count += 1
         _logger.debug('searchCriteria %s from %s', res, filters)
-        return res if res else {'searchCriteria': ''}
+        return res if res else {'searchCriteria': '{}'}
 
 
     def search(self, filters=None):
