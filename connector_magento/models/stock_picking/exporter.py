a64d307372ffcfb6da24a32bd163ba32ddbefc91
Florent THOMAS <florent.thomas@mind-and-go.com>
2019-02-13 07:55:51 +0100

    deal with picking

--- connector_magento/models/stock_picking/exporter.py
+++ connector_magento/models/stock_picking/exporter.py
@@ -71,6 +71,7 @@ class MagentoPickingExporter(Component):
             """
             #FIX https://preprod.unamourdetapis.odoo.mind-and-go.net/web#id=28639&view_type=form&model=queue.job&menu_id=110&action=145
             picking = self.model.browse(binding)
+            _logger.debug("Picking and binding %s / %s" % (picking, binding))
             if picking.magento_id:
                 return _('Already exported')
             lines_info = self._get_lines_info(picking)
