3b7dc2b37d7b17db0b4a8ca3d112071be0225432
Wolfgang Pichler <wpichler@callino.at>
2020-03-03 07:58:09 +0100

    [FIX] Access sale_line direct not from procurement

--- connector_magento/models/stock_picking/exporter.py
+++ connector_magento/models/stock_picking/exporter.py
@@ -37,7 +37,7 @@ class MagentoPickingExporter(Component):
         item_qty = {}
         # get product and quantities to ship from the picking
         for line in binding.move_lines:
-            sale_line = line.procurement_id.sale_line_id
+            sale_line = line.sale_line_id
             if not sale_line.magento_bind_ids:
                 continue
             magento_sale_line = next(
