ca01af99beb71912d61895ec0718cea853d9ef8c
Daniel Duque <daniel.duque@factorlibre.com>
2020-04-29 11:42:36 +0200

    [IMP] connector_magento: Inventory export

--- connector_magento/models/product/importer.py
+++ connector_magento/models/product/importer.py
@@ -367,7 +367,7 @@ class ProductImporter(Component):
 
 
 class ProductInventoryExporter(Component):
-    _name = 'magento.product.product.exporter'
+    _name = 'magento.product.product.inventory.exporter'
     _inherit = 'magento.exporter'
     _apply_on = ['magento.product.product']
     _usage = 'product.inventory.exporter'
